4bb2d6f54c91f330a17a86bee9cc6571f9818452
Olivier Mounicq <olivier.mounicq.it@gmail.com>
2019-05-22 12:15:56 +0200

    Update powershellCmd.ps1

--- powershellCmd.ps1
+++ powershellCmd.ps1
@@ -8,3 +8,45 @@ Get-ChildItem -include "infra.config" -recurse | select-object FullName
 
 Get-ChildItem -include "infra.config" -recurse | %{Add-Content -path $_.FullName "`nthe end`nto"}
 
+Get-ChildItem -Path ".\dir1" -include "infra.config" -recurse | %{Add-Content -path $_.FullName "`nthe end`nto"}
+
+Get-ChildItem -include "infra.config" -recurse | %{Get-Content $_}
+
+
+#Pad Left
+for($i=0; $i -le 10; $i++){ ([string]$i).PadLeft(3,"0")  }
+
+#001
+#002
+#003
+#004
+#005
+#006
+#007
+#008
+#009
+#010
+
+
+$url = "github.com/OlivierMounicq/files/TAG.txt"
+for($i=0; $i -le 10; $i++){ $url -Replace ("TAG", ([string]$i).PadLeft(3,"0"))}
+
+#The output
+#github.com/OlivierMounicq/files/000.txt
+#github.com/OlivierMounicq/files/001.txt
+#github.com/OlivierMounicq/files/002.txt
+#github.com/OlivierMounicq/files/003.txt
+#github.com/OlivierMounicq/files/004.txt
+#github.com/OlivierMounicq/files/005.txt
+#github.com/OlivierMounicq/files/006.txt
+#github.com/OlivierMounicq/files/007.txt
+#github.com/OlivierMounicq/files/008.txt
+#github.com/OlivierMounicq/files/009.txt
+#github.com/OlivierMounicq/files/010.txt
+
+
+
+
+
+
+
