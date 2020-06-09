4e8f2a7b9553ea906212cc7aa0d7529725a242e4
Olivier Mounicq <olivier.mounicq.it@gmail.com>
2020-06-09 14:06:41 +0200

    Create csproj-tooling.ps1

---
+++ csproj-tooling.ps1
@@ -0,0 +1,2 @@
+#The goal : get all config files, copy them and rename them (new name : the project name (the folder of the project) and -packages.conf)
+Get-ChildItem packages.config -recurse | %{ return new-object psobject -property @{Source = $_.FullName; Target = Join-Path E:\Date\2020.06.09\NugetConf (-join((Split-Path(Split-Path  $_.FullName -Parent) -Leaf ), "-packages.config" ))} } | %{ Copy-Item  $_.Source $_.Target}
