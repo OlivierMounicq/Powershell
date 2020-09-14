0c96dce2e7fadafd7f461c0108d984a57cea52ba
Olivier Mounicq <olivier.mounicq.it@gmail.com>
2020-09-14 18:25:31 +0200

    Update CompareTwoProject.ps1

--- CompareTwoProject.ps1
+++ CompareTwoProject.ps1
@@ -1,8 +1,8 @@
 #Purpose : compare two .net project. 
 #You compare the cs, cshtml, config, css, js files
 #You exclude the contnent of the obj and bin folders
-Get-ChildItem E:\GitSource\lmep -File -Recurse | Where { $_.extension -in ".cs",".cshtml",".config",".css",".js" } | Select-Object FullName | %{ if( !(( $_ -like "*\obj\*") -Or ( $_ -like "*\bin\*"))  ){ return $_;} } | %{ return [pscustomobject] @{LmepPoc = $_.FullName; GitSrc = $_.FullName.replace("E:\GitSource\lmep","E:\GitSource\UpsiRetro-Out") }; } | %{ return [pscustomobject] @{ LmepPoc = $_.LmepPoc; GitSrc = $_.GitSrc; IsFileExisting =  [System.IO.File]::Exists($_.GitSrc) } } | %{ if($_.IsFileExisting){ return [pscustomobject] @{ LmepPoc = $_.LmepPoc; GitSrc = $_.GitSrc; AreEqual = ((Get-FileHash $_.LmepPoc).Hash -eq (Get-FileHash $_.GitSrc).Hash)}} } | %{ if(! $_.AreEqual){ return $_;} } | Format-Table -Wrap -Autosize
 
+$refRootPath = "E:\Temp\Project1"
+$rootPath = "E:\Temp\Project2"
 
-
-#
+Get-ChildItem  $refRootPath -File -Recurse  | Where { $_.extension -in ".cs",".cshtml",".config",".css",".js" } | Select-Object FullName | %{ if( !(( $_ -like "*\obj\*") -Or ( $_ -like "*\bin\*"))  ){ return $_;} } | %{ return [pscustomobject] @{ReferenceSrc = $_.FullName; Src = $_.FullName.replace($refRootPath,$rootPath) }; } | %{ return [pscustomobject] @{ ReferenceSrc = $_.ReferenceSrc; Src = $_.Src; IsFileExisting =  [System.IO.File]::Exists($_.Src) } } | %{ if($_.IsFileExisting){ return [pscustomobject] @{ ReferenceSrc = $_.ReferenceSrc; Src = $_.Src; AreEqual = ((Get-FileHash $_.ReferenceSrc).Hash -eq (Get-FileHash $_.Src).Hash)}} else{ return [pscustomobject] @{ ReferenceSrc = $_.ReferenceSrc; Src = "NOT EXIST"; AreEqual = "FALSE" } } } | Export-Csv -delimiter "," -Path E:\Temp\Compare-Proj.csv
