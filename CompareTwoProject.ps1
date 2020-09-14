878568c95d3c58eca5eec66829e12cc809bda91d
Olivier Mounicq <olivier.mounicq.it@gmail.com>
2020-09-14 18:26:01 +0200

    Update CompareTwoProject.ps1

--- CompareTwoProject.ps1
+++ CompareTwoProject.ps1
@@ -6,3 +6,9 @@ $refRootPath = "E:\Temp\Project1"
 $rootPath = "E:\Temp\Project2"
 
 Get-ChildItem  $refRootPath -File -Recurse  | Where { $_.extension -in ".cs",".cshtml",".config",".css",".js" } | Select-Object FullName | %{ if( !(( $_ -like "*\obj\*") -Or ( $_ -like "*\bin\*"))  ){ return $_;} } | %{ return [pscustomobject] @{ReferenceSrc = $_.FullName; Src = $_.FullName.replace($refRootPath,$rootPath) }; } | %{ return [pscustomobject] @{ ReferenceSrc = $_.ReferenceSrc; Src = $_.Src; IsFileExisting =  [System.IO.File]::Exists($_.Src) } } | %{ if($_.IsFileExisting){ return [pscustomobject] @{ ReferenceSrc = $_.ReferenceSrc; Src = $_.Src; AreEqual = ((Get-FileHash $_.ReferenceSrc).Hash -eq (Get-FileHash $_.Src).Hash)}} else{ return [pscustomobject] @{ ReferenceSrc = $_.ReferenceSrc; Src = "NOT EXIST"; AreEqual = "FALSE" } } } | Export-Csv -delimiter "," -Path E:\Temp\Compare-Proj.csv
+
+#
+#
+#
+
+
